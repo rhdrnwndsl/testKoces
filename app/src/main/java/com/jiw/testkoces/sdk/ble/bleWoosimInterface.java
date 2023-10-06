0a620cfff10c244d16dc0652440a86aa7da483d0
rhdrnwndsl <tlswlsdn2457@gmail.com>
2023-10-06 18:16:11 +0900

    일단 여기는 UI 만 모두 처리해 보자

---
+++ app/src/main/java/com/jiw/testkoces/sdk/ble/bleWoosimInterface.java
@@ -0,0 +1,17 @@
+package com.jiw.testkoces.sdk.ble;
+
+import java.util.HashMap;
+
+public class bleWoosimInterface {
+    public interface ConnectionListener {
+        void onState(boolean result);
+    }
+
+    public interface ResDataListener{
+        void OnResult(byte[] res);
+    }
+
+    public interface BLEKeyUpdateListener{
+        void result(String result, String Code, String state, HashMap<String,String> resultData);
+    }
+}
